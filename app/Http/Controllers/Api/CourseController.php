96ffdfff7b8aa27156cf579401bcd4cfe4a6171f
Sowa002 <91929070+Sowa002@users.noreply.github.com>
2024-10-17 03:43:40 +0700

    update

---
+++ app/Http/Controllers/Api/CourseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CourseResource;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::with('category')->get();
+    return CourseResource::collection($courses);
+    }
+
+    public function show($id)
+    {
+        $course = Course::with('category')->findOrFail($id);
+    return new CourseResource($course);
+    }
+
+    public function store(Request $request)
+    {
+        $course = Course::create($request->all());
+        return new CourseResource($course);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $course = Course::findOrFail($id);
+        $course->update($request->all());
+        return new CourseResource($course);
+    }
+
+    public function destroy($id)
+    {
+        Course::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
