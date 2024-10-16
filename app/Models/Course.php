5a24b4144c0d7ff81ef931d127c63de18771bf8f
Demonicky <edotank47@gmail.com>
2024-10-16 16:36:06 +0700

    add course migration and category migration and models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Course extends Model
 {
-    //
+    public function category(): BelongsTo{
+        return $this->belongsTo(Categories::class);
+    }
 }
