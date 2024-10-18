a04c77e99ba61b938755a14a0419bcf0e5a148ef
Demonicky <edotank47@gmail.com>
2024-10-18 19:55:16 +0700

    add new update model

--- app/Models/chapter.php
+++ app/Models/chapter.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class chapter extends Model
 {
-    //
+    public function course(): BelongsTo{
+        return $this->belongsTo(Course::class);
+    }
+
+    public function video(): BelongsTo{
+        return $this->belongsTo(Video::class);
+    }
 }
--- app/Models/video.php
+++ app/Models/video.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class video extends Model
 {
-    //
+    public function chapter(): BelongsTo{
+        return $this->belongsTo(chapter::class);
+    }
 }
